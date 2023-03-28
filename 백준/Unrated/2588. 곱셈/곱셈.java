146737f7ab2133eddace4c3b960a2282549a751f
alswnsdlf <86759172+alswnsdlf@users.noreply.github.com>
2023-03-28 16:50:04 +0900

    [Unrated] Title: 곱셈, Time: 204 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Unrated/2588. 곱셈/곱셈.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        int result1 = A * (B % 10);
+		int result2 = A * (B % 100 / 10);
+		int result3 = A * (B % 1000 / 100);
+
+		System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+		System.out.println(result1 + (result2 * 10) + (result3 * 100));
+        
+        sc.close();
+    }
+}
